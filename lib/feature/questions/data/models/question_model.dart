d8d88f1a190b0e0b2d48f80c36f306e30b8d8ec9
Ahmed <benzemaa46@gmail.com>
2026-02-16 14:52:28 +0200

    Add question models

--- lib/feature/questions/data/models/question_model.dart
+++ lib/feature/questions/data/models/question_model.dart
@@ -1,1 +1,41 @@
+class QuestionModel {
+  int responseCode;
+  List<ResultModel> result;
+  QuestionModel({required this.responseCode, required this.result});
 
+  factory QuestionModel.fromJson(Map<String, dynamic> json) {
+    return QuestionModel(
+      responseCode: json['response_code'],
+      result: (json['results'] as List)
+          .map((e) => ResultModel.fromJson(e))
+          .toList(),
+    );
+  }
+}
+
+class ResultModel {
+  String question;
+  String correctAnswer;
+  List<String> incorrectAnswers;
+  List<String> allAnswers;
+  ResultModel({
+    required this.question,
+    required this.correctAnswer,
+    required this.incorrectAnswers,
+    required this.allAnswers,
+  });
+
+  factory ResultModel.fromJson(Map<String, dynamic> json) {
+    String correct = json['correct_answer'];
+    List<String> incorrect = List<String>.from(json['incorrect_answers']);
+
+    List<String> combined = [correct, ...incorrect]; // all answers combined
+    combined.shuffle(); // shuffle the list to randomize the answers
+    return ResultModel(
+      question: json['question'],
+      correctAnswer: correct,
+      incorrectAnswers: incorrect,
+      allAnswers: combined,
+    );
+  }
+}
